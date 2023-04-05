083141eab2cc1dd9d09d0d0810f1f0c3cc86be49
Olena Pashchenko <helen.watermore@gmail.com>
2023-04-05 13:50:43 -0700

    Relocated fs.writeFile() method from the inquirer.prompt() to the  writeToFile().

--- Develop/index.js
+++ Develop/index.js
@@ -5,30 +5,31 @@ const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [{
-    type: 'input',
-    message: 'What is the title of your project?',
     name: 'title',
+    message: 'What is the title of your project?',
+    type: 'input',
   },
 ]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('success');
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {
-
     inquirer.prompt(
         questions
     )
     .then(function(data) {
         console.log(data)
-        fs.writeFile('README.md', generateMarkdown({...data}), (err) => {
-            if (err) {
-              console.log(err);
-              return;
-            }
-            console.log('success');
-        })
+        writeToFile('README.md', data)
     })
 }
 
